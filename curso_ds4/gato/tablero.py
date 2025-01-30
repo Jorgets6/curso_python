668a7f42cc376f32b032c61ec0e2ef60527ed848
Jorgets6 <terrazasserna11@hotmail.com>
2025-01-30 14:23:44 -0700

    Se agrego una ia

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -14,10 +14,22 @@ def dibuja_tablero(simbolos:dict):
           {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
           ''')
     
+def ia(simbolos:dict):
+    '''Estrategia para la computadora'''
+    ocupado = True
+    while ocupado in True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X', 'O']:
+            simbolos[x] = 'O'
+            ocupado = False
+    
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
+    ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    ''' 
     x = random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x] = 'X'
@@ -25,5 +37,6 @@ if __name__ == '__main__':
     o = random.choice(numeros)
     numeros.remove(o)
     dsimbolos[0] = 'O'
+    '''
     dibuja_tablero(dsimbolos)
     print(numeros)
