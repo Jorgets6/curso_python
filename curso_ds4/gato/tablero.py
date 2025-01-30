8e71ea42a9ff328f6f94ebda25ea361370388e94
Jorgets6 <terrazasserna11@hotmail.com>
2025-01-30 14:35:34 -0700

    Agruegue la estrategia del usuario.

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -17,17 +17,34 @@ def dibuja_tablero(simbolos:dict):
 def ia(simbolos:dict):
     '''Estrategia para la computadora'''
     ocupado = True
-    while ocupado in True:
+    while ocupado is True:
         x = random.choice(list(simbolos.keys()))
         if simbolos[x] not in ['X', 'O']:
             simbolos[x] = 'O'
             ocupado = False
+            
+def usuario(simbolos:dict):
+    '''Estrategias del usuario'''
+    ocupado = True
+    lista_numeros = [str(i) for i in range(1,10)]
+    while ocupado is True:
+        x = input('Elija un número del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                if simbolos[x] not in ['X','O']:
+                    simbolos[x] = 'X'
+                    ocupado = False
+            else:
+                    print('Esa casilla ya esta ocupada')
+                    
     
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
     ''' 
     x = random.choice(numeros)
@@ -36,7 +53,4 @@ if __name__ == '__main__':
     dibuja_tablero(dsimbolos)
     o = random.choice(numeros)
     numeros.remove(o)
-    dsimbolos[0] = 'O'
-    '''
-    dibuja_tablero(dsimbolos)
-    print(numeros)
+    dsimbolos[0] = 'O' '''
