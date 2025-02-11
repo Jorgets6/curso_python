d4c228a864452f8a6d5a848822817a30d43db99d
Jorgets6 <terrazasserna11@hotmail.com>
2025-02-11 14:37:00 -0700

    Se agrego un archivo a datos, y al programa funciones

--- Ahorcado/funciones.py
+++ Ahorcado/funciones.py
@@ -4,15 +4,40 @@ Funciones auxiliares para el juego del ahorcado
 
 def carga_archivo_texto(archivo:str)->list:
     '''
-    Carga un archivo de textp y devuelve una lista con las otaciones del archivo
+    Carga un archivo de texto y devuelve una lista con las otaciones del archivo
     '''
     
-    with open(archivo, 'r', encoding='utf-8')as file: oraciones = file.readlines()
+    with open(archivo, 'r', encoding='utf-8')as file: 
+        oraciones = file.readlines()
     return oraciones
 
+def carga_plantillas(nombre_plantilla:str)->dict:
+    '''
+    Carga las plantillas del juego ahorcado de un documento de texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}_{i}.txt' )
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+    return diccionario[nivel]
+
+
 if __name__ == '__main__':
-    lista = carga_archivo_texto('./plantillas/plantilla_0.txt')
-    for elemento in lista:
-        print(elemento)
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    print(lista_oraciones[120:150])
+    texto = ' '.join(lista_oraciones[1202:150])
+    print(texto)
+    
         
         
